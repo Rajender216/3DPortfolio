b8fcb894e1c5aca2f71a0ade296a543c1bb704ec
Rajender216 <137612714+Rajender216@users.noreply.github.com>
2025-02-24 19:57:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 5173, // Use Render's port if available
-    host: "0.0.0.0", // Allow external connections
+   server: {
+    port: process.env.PORT || 5173,  // Ensure Render can assign a port
+    host: '0.0.0.0',  // Allow external access
+    strictPort: true,
+    allowedHosts: ['threedportfolio-72pm.onrender.com'], // Add your Render domain
   },
   
   build: {
