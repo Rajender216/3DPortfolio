6efd4c327003e9d71c9dcf2d5d02daffa641afbd
{Rajender216} <216rajender@gmail.com>
2025-02-24 23:25:05 +0530

    refactor Home component layout and enhance text styling; clean up vite.config.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ import "../index.css";
 
 export default function Home() {
   return (
-    <div id="home" className="min-h-screen bg-black pt-6 md:pt-36 flex flex-col-reverse md:flex-row items-center justify-center md:justify-between gap-6 md:gap-10 px-4 md:px-8 lg:px-16 border-b-4 border-teal-800">
+    <div
+      id="home"
+      className="min-h-screen bg-black pt-6 md:pt-36 flex flex-col-reverse md:flex-row items-center justify-center md:justify-between gap-6 md:gap-10 px-4 md:px-8 lg:px-16 border-b-4 border-teal-800"
+    >
       {/* Spline/Homebg Container with Aspect Ratio */}
       <div className="w-full md:w-1/2 lg:w-[45%] flex items-center justify-center p-4">
         <div className="relative w-full" style={{ paddingTop: "56.25%" }}>
@@ -17,8 +20,11 @@ export default function Home() {
       </div>
 
       {/* Text Section */}
-      
+
       <div className="w-full md:w-1/2 lg:w-[45%] text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold p-4 md:p-6 rounded-lg text-center md:text-left shadow-[0_20px_50px_rgba(0,255,255,0.3)] hover:shadow-[0_25px_60px_rgba(0,255,255,0.4)]">
+        <h1 className="text-5xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 mb-4">
+          Hello!  I am 
+        </h1>
         <h1 className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
           Rajender Chhimpa
         </h1>
--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     port: process.env.PORT || 5173, // Use Render's port if available
     host: "0.0.0.0", // Allow external connections
   },
-=======
-   server: {
-    port: process.env.PORT || 5173,  // Ensure Render can assign a port
-    host: '0.0.0.0',  // Allow external access
-    strictPort: true,
-    allowedHosts: ['threedportfolio-72pm.onrender.com'], // Add your Render domain
-  },
-  
->>>>>>> 6b314f972e9f1e817171799f2174b99b7e7a3920
   build: {
     chunkSizeWarningLimit: 2500, // Set to 1500 kB
   },
