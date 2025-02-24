49fe385a7a443966fa15736efc0d5822f9c06827
{Rajender216} <216rajender@gmail.com>
2025-02-24 23:28:52 +0530

    update vite.config.js to enforce strict port usage and specify allowed hosts for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173, // Use Render's port if available
-    host: "0.0.0.0", // Allow external connections
+    port: process.env.PORT || 5173, // Ensure Render can assign a port
+    host: "0.0.0.0", // Allow external access
+    strictPort: true,
+    allowedHosts: ["threedportfolio-72pm.onrender.com"], // Add your Render domain
   },
   build: {
     chunkSizeWarningLimit: 2500, // Set to 1500 kB
