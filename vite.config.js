b7d311ce96606675df0bd7344feed49581e5af7f
{Rajender216} <216rajender@gmail.com>
2025-02-24 19:49:04 +0530

    configure Vite server settings and increase chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's port if available
+    host: "0.0.0.0", // Allow external connections
+  },
   build: {
-    chunkSizeWarningLimit: 1500, // Set to 1500 kB
+    chunkSizeWarningLimit: 2500, // Set to 1500 kB
   },
 });
