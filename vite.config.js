f9e0833d30c32166a910cfabceb1906695817f19
Rajender216 <137612714+Rajender216@users.noreply.github.com>
2025-02-24 19:50:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's port if available
+    host: "0.0.0.0", // Allow external connections
+  },
   
   build: {
     chunkSizeWarningLimit: 2500, // Set to 1500 kB
